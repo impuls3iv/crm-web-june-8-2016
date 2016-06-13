8d36ed02176f83f6ef18835d331fb8dafad7eb42
Sam <sammy.unterman@gmail.com>
2016-06-13 11:32:04 -0400

    added field lines for class

--- contact.rb
+++ contact.rb
@@ -12,57 +12,21 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'CRM_web.s
 
 class Contact < ActiveRecord::Base
 
-
-
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-
-
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-
-
-
-
-
-  # This method should delete all of the contacts
-
+#-------------------------------
+#creates field with symbol as column name, as as key, and datatype
+  field :first_name, as: :string
+  field :last_name, as: :string
+  field :email, as: :string
+  field :note,  as: :text
+#dont need an id field because sqlite creates field for you
+#----------------------------------
 
   def full_name
 
     #return @contact
-    return "#{@first_name} #{@last_name}"
+    return "#{ @first_name } #{ @last_name }"
 
   end
 
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
-
-    #@@contacts.delete(self)
-    # delete_var = delete_id.to_i #do i need to do this? who knows...
-    # @@contacts.each do |item| #iterate over each instance of contact in the master array
-    #     if item.id == delete_var #cif there is an item id thats the same as user inputted delete_var
-    #       temp_var = @@contacts.index(item) #save the index number of the @@contact instance with the id
-    #       @@contacts.delete_at(temp_var) #and delete the array item at that index number
-    #     end
-
-        # puts "#{temp_var}"
-
-
-
-      # var = @@contacts
-      # puts @@contact(contact_delete)
-      # @@contacts.delete_at(contact_delete)
-
-    #end
-
-  # Feel free to add other methods here, if you need them.
 
 end
-
-#
-# jon = Contact.create('jon', 'doe', 'jon@sam.com', 'note22222')
-# sam = Contact.create('sam', 'unt', 'sam@sam.com', 'note1')
-# jane = Contact.create('jane', 'smith', 'jane@gmail.com', 'sooo cool')
